c900228d80ca135313825c37092cd06df0c95651
LamLeTomat <61313056+LamLeTomat@users.noreply.github.com>
2021-05-18 14:41:59 +0700

    Call API with useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import PostList from './components/PostList';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
@@ -10,7 +11,32 @@ function App() {
     {id: 1, title: 'Đi học'},
     {id: 2, title: 'Đi làm'},
     {id: 3, title: 'Đi chơi'},
-  ])
+  ]);
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1`;
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        
+        const data = responseJSON.data; // == const {data} = responseJSON 
+        setPostList(data)
+      } catch (error) {
+        console.log('Failed to fetch', error.message)
+      }
+      
+    }
+    console.log('Post list effect')
+    fetchPostList();
+
+  },[]);
+
+  useEffect(() => {
+    console.log('Todolist effect');
+  });
+
   function handleTodoClick(todo){
     console.log(todo);
     const index  = todoList.findIndex(x => x.id === todo.id);
@@ -26,13 +52,18 @@ function App() {
   
   return (
     <div className="app">
-      <h1>React hooks TodoList</h1>
-      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <h1>React hooks PostLisssst</h1>
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+
       {/* <ColorBox/> */}
-      <TodoList 
+      
+      {/* <TodoList 
         todos={todoList}
         onTodoClick={handleTodoClick} 
-      />
+      /> */}
+
+      <PostList posts={postList} />
+
     </div>
   );
 }
