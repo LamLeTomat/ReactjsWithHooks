aba6aceb6b49fcc77b5a0b058de8b86dc8fe53a9
LamLeTomat <61313056+LamLeTomat@users.noreply.github.com>
2021-05-20 11:51:02 +0700

    useEffect Pagination

--- src/App.js
+++ src/App.js
@@ -1,58 +1,78 @@
 import { useEffect, useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+// import ColorBox from './components/ColorBox';
+import Pagination from './components/Pagination';
 import PostList from './components/PostList';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+// import TodoForm from './components/TodoForm';
+// import TodoList from './components/TodoList';
+import queryString from 'query-string';
 
 function App() {
   //Khởi tạo mảng giá trị
-  const [todoList, setTodoList] = useState([
-    {id: 1, title: 'Đi học'},
-    {id: 2, title: 'Đi làm'},
-    {id: 3, title: 'Đi chơi'},
-  ]);
+  // const [todoList, setTodoList] = useState([
+  //   {id: 1, title: 'Đi học'},
+  //   {id: 2, title: 'Đi làm'},
+  //   {id: 3, title: 'Đi chơi'},
+  // ]);
   const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page : 1,
+    _litmit : 10,
+    _totalRows: 1,
+  });
+
+  const [filters, setFilters] = useState({
+    _litmit: 10,
+    _page: 1,
+  })
 
   useEffect(() => {
     async function fetchPostList() {
       try {
-        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1`;
+        const paramString = queryString.stringify(filters)
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramString}`;
         const response = await fetch(requestUrl);
         const responseJSON = await response.json();
         
-        const data = responseJSON.data; // == const {data} = responseJSON 
+        const {data, pagination} = responseJSON;
+        setPagination(pagination) 
         setPostList(data)
       } catch (error) {
         console.log('Failed to fetch', error.message)
       }
       
     }
-    console.log('Post list effect')
     fetchPostList();
 
-  },[]);
+  },[filters]);
 
-  useEffect(() => {
-    console.log('Todolist effect');
-  });
+  // useEffect(() => {
+  //   console.log('Todolist effect');
+  // });
 
-  function handleTodoClick(todo){
-    console.log(todo);
-    const index  = todoList.findIndex(x => x.id === todo.id);
-    if (index < 0 ) return; //Nếu k tìm thấy index thì trả về -1
-    const newTodoList = [...todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  }
+  // function handleTodoClick(todo){
+  //   console.log(todo);
+  //   const index  = todoList.findIndex(x => x.id === todo.id);
+  //   if (index < 0 ) return; //Nếu k tìm thấy index thì trả về -1
+  //   const newTodoList = [...todoList];
+  //   newTodoList.splice(index, 1);
+  //   setTodoList(newTodoList);
+  // }
 
-  function handleTodoFormSubmit(formValues) {
-    console.log('Form Submit:', formValues )
+  // function handleTodoFormSubmit(formValues) {
+  //   console.log('Form Submit:', formValues )
+  // }
+
+  function handlePageChange(newPage) {
+    setFilters({
+      ...filters,
+      _page: newPage,
+    })
   }
   
   return (
     <div className="app">
-      <h1>React hooks PostLisssst</h1>
+      <h1>React hooks Pagination</h1>
       {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
 
       {/* <ColorBox/> */}
@@ -63,6 +83,7 @@ function App() {
       /> */}
 
       <PostList posts={postList} />
+      <Pagination pagination={pagination} onPageChange={handlePageChange}/>
 
     </div>
   );
