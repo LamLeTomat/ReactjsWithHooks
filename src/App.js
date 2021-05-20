41045f3cdbd4baacd3ed97f1bc8a665ef9f00d63
LamLeTomat <61313056+LamLeTomat@users.noreply.github.com>
2021-05-20 14:57:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ function App() {
   const [postList, setPostList] = useState([]);
   const [pagination, setPagination] = useState({
     _page : 1,
-    _litmit : 10,
+    _limit : 10,
     _totalRows: 1,
   });
 
   const [filters, setFilters] = useState({
-    _litmit: 10,
+    _limit: 6,
     _page: 1,
   })
 
@@ -30,6 +30,7 @@ function App() {
     async function fetchPostList() {
       try {
         const paramString = queryString.stringify(filters)
+        console.log(paramString)
         const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramString}`;
         const response = await fetch(requestUrl);
         const responseJSON = await response.json();
